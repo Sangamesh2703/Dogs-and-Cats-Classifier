27ef5c3dd053e702147321f32660c84180ce0da3
Sangamesh Mainale <01fe20bcs060@kletech.ac.in>
2022-09-02 19:36:26 +0530

    cd

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper() 
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+#cell
+im = PILImage.create('dog.jpg')
+im.thumbnail((192,192))
+
+#cell
+learn = load_learner('model.pkl')
+learn.predict(im)
+
+#cell
+categories = ('Dog','Cat')
+
+def classify_images(img):
+  pred,idx,probs = learn.predict(img)
+  return dict(zip(categories, map(float,probs)))
+
+
+#cell
+image = gr.inputs.Image(shape=(192,192))
+label = gr.outputs.Label()
+
+intf = gr.Interface(fn=classify_images, inputs=image, outputs=label)
+intf.launch(inline=False)
+
