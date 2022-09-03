76742f9e0c6680e3bb6ef96ab12fdbda70e6c8f3
Sangamesh Mainale <01fe20bcs060@kletech.ac.in>
2022-09-03 22:42:52 +0530

    dogs_cats

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+#|default_expv app
+
+#cell
+import numpy as np
+
+#cell
+from fastai.vision.all import *
+
+#cell
+#|export
 from fastai.learner import load_learner
 from PIL import Image
 import gradio as gr
@@ -7,23 +17,38 @@ def is_cat(x): return x[0].isupper()
 #cell
 im = PILImage.create('dog.jpg')
 im.thumbnail((192,192))
+im
 
 #cell
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#cell
+#|export
 learn = load_learner('model.pkl')
+
+#cell
 learn.predict(im)
 
 #cell
+#|export
 categories = ('Dog','Cat')
 
 def classify_images(img):
   pred,idx,probs = learn.predict(img)
   return dict(zip(categories, map(float,probs)))
 
+  #cell
+  classify_images(im)
 
-#cell
+  #cell
+  #|export
 image = gr.inputs.Image(shape=(192,192))
 label = gr.outputs.Label()
 
 intf = gr.Interface(fn=classify_images, inputs=image, outputs=label)
 intf.launch(inline=False)
 
+#cell
+m = learn.model
